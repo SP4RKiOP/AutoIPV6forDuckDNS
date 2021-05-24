ad9cfce0ede491e4a8a23f2b28b05e79f9d29f0d
Abhishek Sinha <abhisheksp4rk@gmail.com>
2021-05-25 02:50:12 +0530

    Update autoipv6duckdns.sh

--- autoipv6duckdns.sh
+++ autoipv6duckdns.sh
@@ -1,11 +1,10 @@
 #Presented to you by Abhishek Sinha
 #Fully Automation of ipv6 address update to DUCKDNS
 
-#ipv4 address variable
-ipv4add=$(curl -4 icanhazip.com)
+ipv4add=$(curl --ipv4 https://gtwy.net/ip/)
 
 #ipv6 address variable
-ipv6add=$(curl -6 icanhazip.com)
+ipv6add=$(curl --ipv6 https://gtwy.net/ip/)
 
 #Url to update the gathered information by variables to duckdns server.
 echo url="https://www.duckdns.org/update?domains=YourDOMAIN&token=YourTOKEN&ip=$ipv4add&ipv6=$ipv6add&verbose=true"$ | curl -k -o ~/autoipv6duckdns/autoipv6duckdns.log -K -
