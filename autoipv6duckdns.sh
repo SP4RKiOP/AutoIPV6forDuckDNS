6c019e64eac281d4bed64102add45adb9f0be1b8
Abhishek Sinha <68564589+SP4RKiOP@users.noreply.github.com>
2024-03-06 03:28:19 +0530

    Fixed #1
    #1 fixed, removed unattended flag

--- autoipv6duckdns.sh
+++ autoipv6duckdns.sh
@@ -7,6 +7,4 @@ ipv4add=$(curl --ipv4 https://gtwy.net/ip/)
 ipv6add=$(curl --ipv6 https://gtwy.net/ip/)
 
 #Url to update the gathered information by variables to duckdns server.
-echo url="https://www.duckdns.org/update?domains=YourDOMAIN&token=YourTOKEN&ip=$ipv4add&ipv6=$ipv6add&verbose=true" | curl -k -o ~/autoipv6duckdns/autoipv6duckdns.log -K -
-
-
+echo url="https://www.duckdns.org/update?domains=YourDOMAIN&token=YourTOKEN&ip=$ipv4add&ipv6=$ipv6add&verbose=true" | curl -k -o ~/autoipv6duckdns/autoipv6duckdns.log
